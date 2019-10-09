a15836c991182dcb606c4340f911443fd1fceddc
StinsonOvO <tingsheng.shi@gmail.com>
2019-10-10 00:52:06 +0800

    added explicit intent

--- app/src/main/java/com/ync/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/ync/msgshareapp/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
             startActivity(intent)
         }
 
