fb1383345072f8b6cbcbb218e58d5a41e2091d3f
StinsonOvO <tingsheng.shi@gmail.com>
2019-10-10 01:35:30 +0800

    added model class

---
+++ app/src/main/java/com/ync/msgshareapp/Model.kt
@@ -0,0 +1,44 @@
+package com.ync.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier {
+
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Waling"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Waling"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Waling"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Waling"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Waling"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking")
+    )
+}
